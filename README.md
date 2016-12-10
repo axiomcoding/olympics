f55bc79e0bf4f7f75770d7aac25c51fa90ec4cc2
Michael Tejada <michael@axiomcoding.com>
2016-12-09 21:50:06 -0700

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# Olympics
+Use array methods and enumeration in `olympics.rb` to pass each RSpec test in `olympics_spec.rb`.  
+
+![1](http://i.imgur.com/okrHgXF.gif)
---
+++ olympics.rb
@@ -0,0 +1,26 @@
+# define a method, first_event, that takes in an events array as an argument
+# return the first item in the array
+
+
+
+
+
+# define a method, remove_first, that takes in an events array as an argument
+# remove the first item from the array
+
+
+
+
+
+# define a method, update_event_list, that takes in an events array as an argument
+# add the event "cycling" to the end of the array
+
+
+
+
+
+
+# define a method, order_events, that takes in an events array as an argument
+# your goal is to iterate through each array index value and return a manipulated array
+# the manipulated array will list the items with their index value in the following format
+# example: ["1. Hurdles", "2. Basketball", etc.]
